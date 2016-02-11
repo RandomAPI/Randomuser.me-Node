41c65963381df3b66082435f13898e6050705b10
Keith Armstrong <keith1015@gmail.com>
2016-02-11 17:42:56 -0600

    ci demo

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 
 gulp.task('default', function () {
-	return gulp.src('spec/randomuserTests.js', {read: false})
-		// gulp-mocha needs filepaths so you can't have any plugins before it
-		.pipe(mocha({reporter: 'nyan'}));
+  return gulp.src('spec/randomuserTests.js', {read: false})
+    // gulp-mocha needs filepaths so you can't have any plugins before it
+    .pipe(mocha({reporter: 'nyan'}));
 });
