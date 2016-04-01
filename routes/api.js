71bc0a3475eb5deeeeb34e34436bd94356cafc60
Keith Armstrong <keith1015@gmail.com>
2016-04-01 15:51:56 -0500

    Added support for easily adding new versions

--- routes/api.js
+++ routes/api.js
@@ -4,21 +4,40 @@ var router  = express.Router();
 var cors    = require('cors');
 var Request = require('../models/Request');
 
+var latestVersion = "1.0";
+
 router.get('/', cors(), function(req, res, next) {
+  genUser(req, res, "1.0");
+});
+
+router.get('/:version', cors(), function(req, res, next) {
+  genUser(req, res, req.params.version);
+});
+
+function genUser(req, res, version) {
+  version = version || latestVersion;
+  if (typeof Generator[version] === 'undefined') {
+    res.sendStatus(404);
+    return;
+  }
   var results = req.query.results || 1;
-  var output  = new Generator(req.query).generate();
-  Request.findOrCreate({date: getDateTime()}, {"1_0": results, "bandwidth": output.length}, function(err, obj, created) {
+  var output  = new Generator[version](req.query).generate();
+
+  var payload = {"bandwidth": output.length};
+  payload[version] = results;
+
+  Request.findOrCreate({date: getDateTime()}, payload, function(err, obj, created) {
     res.setHeader('Content-Type', 'application/json');
     // Update record
     if (!created) {
-      Request.update({date: getDateTime()}, {$inc: {"1_0": results, "bandwidth": output.length}}, function(err, blah, a) {
+      Request.update({date: getDateTime()}, {$inc: payload}, function(err, blah, a) {
         res.send(output);
       });
     } else {
       res.send(output);
     }
   });
-});
+}
 
 module.exports = router;
 
