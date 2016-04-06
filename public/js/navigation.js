fb024cbf0ef203c6906547c2a8219e4ca7e37208
Keith Armstrong <keith1015@gmail.com>
2016-04-06 14:53:23 -0500

    Updated navbar

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -3,6 +3,11 @@
     addListenerMulti(document.getElementsByClassName('nav_toggle')[0], 'touchstart click', function() {
       document.getElementsByTagName('body')[0].classList.toggle('active');
     });
+    window.addEventListener('resize', function() {
+      if (document.body.clientWidth > 1145) {
+        document.getElementsByTagName('body')[0].className = document.getElementsByTagName('body')[0].className.replace(/\bactive\b/,'');
+      }
+    })
   });
 
   function addListenerMulti(el, s, fn) {
