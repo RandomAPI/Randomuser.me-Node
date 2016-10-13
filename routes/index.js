424f6a6644bb19c5ae6746593584931026437c7a
Keith Armstrong <keith1015@gmail.com>
2016-10-12 22:23:05 -0500

    Fix Stripe donation page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var express  = require('express');
 var router   = express.Router();
 var settings = require('../settings');
 
+var stripe = require("stripe")(
+  settings.stripePrivateKey
+);
+
 var views;
 fs.readdir('.viewsMin/pages', function(err, data) {;
   views = data;
@@ -30,10 +34,25 @@ router.get('/:page?', function(req, res, next) {
   if (page === 'index') {
     res.redirect(301, '/');
   } else if (views.indexOf(page + '.ejs') !== -1) {
-    res.render('pages/' + page, {socket: ':' + settings.socket, title: titles[page]});
+    res.render('pages/' + page, {socket: ':' + settings.socket, title: titles[page], stripePublishKey: settings.stripePublishKey});
   } else {
     next();
   }
+});
+
+router.post('/donate', function(req, res, next) {
+  let data = JSON.parse(req.body.data);
+
+  stripe.charges.create({
+    amount: data.price,
+    currency: "usd",
+    source: data.id, // obtained with Stripe.js
+    description: "Donation from " + data.email
+  }, function(err, charge) {
+    if (err) return res.sendStatus(400);
+    else res.sendStatus(200);
+  });
+
 });
 
 router.all('/download/version.php', function(req, res, next) {
