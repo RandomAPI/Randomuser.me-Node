4e2a3471e89805e3777cb6f9814bfc9175404539
Keith Armstrong <keith1015@gmail.com>
2019-10-10 18:57:28 -0500

    Remove spaces from email address #133

--- spec/api/modern/1.3.js
+++ spec/api/modern/1.3.js
@@ -5,6 +5,19 @@ const defVersion = "1.3";
 
 module.exports = (server, version=defVersion) => {
 
+  describe(`Email address formatting`, () => {
+
+    // No spaces in email address
+    it(`should not include spaces in email address`, async () => {
+      const res = await request(server).get(`/api/${version}?results=5000&inc=email`);
+      const result = JSON.parse(res.text);
+
+      expect(result.results.every(user => {
+        return user.email.indexOf(' ') === -1;
+      })).to.equal(true);
+    });
+  });
+
   // 1.3 should support all previous version features as well
   describe(`Checking support of previous version features`, () => {
     describe('1.2', require('./1.2').bind(this, server, version));
