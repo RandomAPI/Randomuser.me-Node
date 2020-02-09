7e2439509dc4457f737cfe10dcbeeaa026759110
Robin P <robin.w.pollard@gmail.com>
2020-02-09 14:20:14 +0100

    Changed Phone, GSM and Postcode to follow the Dutch standards

--- api/1.3/data/NL/inject.js
+++ api/1.3/data/NL/inject.js
@@ -4,11 +4,21 @@ module.exports = (inc, contents) => {
     const pic = contents.picture;
     delete contents.picture;
 
-    include(inc, contents, 'phone', '(' + random(3, 3) + ')-' + random(3, 3) + '-' + random(3, 4));
-    include(inc, contents, 'cell', '(' + random(3, 3) + ')-' + random(3, 3) + '-' + random(3, 4));
+    include(inc, contents, 'phone', () => {
+      const phones = [
+        '(0' + random(3, 2) + ') ' + random(3, 7),
+        '(0' + random(3, 3) + ') ' + random(3, 6)
+      ];
+      contents.phone = randomItem(phones);
+    });
+    include(inc, contents, 'cell', '(06) ' + random(3, 8));
     include(inc, contents, 'id', {
         name: 'BSN',
         value: random(3, 8)
+    });
+    include(inc, contents, 'location', () => {
+    const code = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    contents.location.postcode = random(3, 4) + ' ' + code[range(0, 25)] + code[range(0, 25)];
     });
     include(inc, contents, 'picture', pic);
 };
