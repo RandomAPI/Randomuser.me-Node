1c46ead062165b6d6a0c8a4af6d73c41deaa13de
Jambaldorj Ochirpurev <dorj222@gmail.com>
2020-07-25 23:02:29 -0400

    Update inject.js

--- api/nextRelease/data/MN/inject.js
+++ api/nextRelease/data/MN/inject.js
@@ -5,8 +5,8 @@ module.exports = (inc, contents) => {
     const pic = contents.picture;
     delete contents.picture;
 
-    include(inc, contents, 'phone', '(' + random(3, 3) + ')-' + random(3, 3) + '-' + random(3, 4));
-    include(inc, contents, 'cell', '(' + random(3, 3) + ')-' + random(3, 3) + '-' + random(3, 4));
+    include(inc, contents, 'phone',  976 + '-' + random(3, 8));
+    include(inc, contents, 'cell',  976 + '-' + random(3, 8));
     include(inc, contents, 'id', {
         name: 'RN',
         value: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 2) + random(3,8);
