cae91bd1449bc72bb4a79a137f85cc30dfdb608e
Michael Nti <michael.nti@aexp.com>
2020-07-29 10:45:05 -0400

    Added US Income Source

--- api/.nextRelease/data/US/inject.js
+++ api/.nextRelease/data/US/inject.js
@@ -4,11 +4,20 @@ module.exports = (inc, contents) => {
     const pic = contents.picture;
     delete contents.picture;
 
-    include(inc, contents, 'phone', '(' + random(4, 1) + random(3, 2) + ') ' + random(4, 1) + random(3, 2) + '-' + random(3, 4));
-    include(inc, contents, 'cell', '(' + random(4, 1) + random(3, 2) + ') ' + random(4, 1) + random(3, 2) + '-' + random(3, 4));
+    let phone =  '(' + random(4, 1) + random(3, 2) + ') ' + random(4, 1) + random(3, 2) + '-' + random(3, 4);
+    let cell = '(' + random(4, 1) + random(3, 2) + ') ' + random(4, 1) + random(3, 2) + '-' + random(3, 4);
+    let annualIncome = range(1000, 10000000);
+    let nonTaxableIncome = range(0, annualIncome - 1);
+   
+    include(inc, contents, 'phone', phone);
+    include(inc, contents, 'cell', cell);
     include(inc, contents, 'id', {
         name: 'SSN',
         value: random(3, 3) + '-' + random(3, 2) + '-' + random(3, 4)
     });
+    include(inc, contents, 'income', {
+        annualIncome: annualIncome,
+        nonTaxableIncome
+    })
     include(inc, contents, 'picture', pic);
 };
