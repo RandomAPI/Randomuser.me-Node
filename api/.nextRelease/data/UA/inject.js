c49c10c1b2e15bf4140a9cd80b26175740693884
olesh <olegkirashchuk@gmail.com>
2021-01-26 07:12:05 +0200

    transliterate the data

--- api/.nextRelease/data/UA/inject.js
+++ api/.nextRelease/data/UA/inject.js
@@ -1,13 +1,13 @@
 const {random, randomItem, include,} = require('../../api');
 
 module.exports = (inc, contents) => {
-    const mobileProviders = ['066', '067', '068', '096', '097', '098', '099'];
+    const mobileProviders = ['66', '67', '68', '96', '97', '98', '99'];
 
     const pic = contents.picture;
     delete contents.picture;
 
-    include(inc, contents, 'phone', '(' + randomItem(mobileProviders) + random(3, 2) + ') ' + random(4, 1) + random(3, 2) + '-' + random(3, 4));
-    include(inc, contents, 'cell', '(' + random(4, 1) + random(3, 2) + ') ' + random(4, 1) + random(3, 2) + '-' + random(3, 4));
+    include(inc, contents, 'phone', '(0' + randomItem(mobileProviders) + ') ' + random(4, 1) + random(3, 2) + '-' + random(3, 4));
+    include(inc, contents, 'cell', '(0' + randomItem(mobileProviders) + ') ' + random(4, 1) + random(3, 2) + '-' + random(3, 4));
     include(inc, contents, 'id', {
         name: '',
         value: null
