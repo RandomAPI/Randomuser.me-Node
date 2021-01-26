e21f1fad26b7c5d712292f4ff144fbfdd849297a
dorj222 <dorj222@gmail.com>
2021-01-26 00:29:37 -0500

    add Mongolia

--- api/.nextRelease/data/MN/inject.js
+++ api/.nextRelease/data/MN/inject.js
@@ -9,7 +9,7 @@ module.exports = (inc, contents) => {
     include(inc, contents, 'cell',  976 + '-' + random(3, 8));
     include(inc, contents, 'id', {
         name: 'RN',
-        value: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 2) + random(3,8);
+        value: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 2) + random(3,8)
     });
     include(inc, contents, 'picture', pic);
 };
